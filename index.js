2f3abe004c905d252ee7ccfc7ca0bc23f9a3a531
Willian <willian.alves.b15@gmail.com>
2022-04-25 19:17:03 -0300

    feat: req 4

--- index.js
+++ index.js
@@ -35,7 +35,21 @@ app.get('/talker/:id', (req, res) => {
   });
 });
 
-app.post('/login', (req, res) => {
+function validateEmail(req, res, next) {
+  const { email, password } = req.body;
+  const emailRegex = /[a-z0-9]+[@]+[a-z]+[.]+[a-z]/;
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+}
+
+app.post('/login', validateEmail, (req, res) => {
   try {
     const { email, password } = req.body;
     const token = crypto.randomBytes(8).toString('hex');
