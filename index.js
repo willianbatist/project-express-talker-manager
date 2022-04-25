d6bf6583880ed3ee9adfbe73cd085bf0358aca61
Willian <willian.alves.b15@gmail.com>
2022-04-25 18:15:10 -0300

    feat: req 3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs').promises;
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -32,4 +33,18 @@ app.get('/talker/:id', (req, res) => {
     if (!talker) res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
     return res.status(200).json(talker);
   });
+});
+
+app.post('/login', (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const token = crypto.randomBytes(8).toString('hex');
+    const valitadion = [email, password].includes(undefined);
+    if (!valitadion) {
+      return res.status(200).json({ token });
+    }
+    return res.status(401).json({ message: 'missing fields' });
+  } catch (error) {
+    return res.status(500).end();
+  }
 });
